e763917970216fbb1110b19b4e807f22fd79584b
matheusosp <matheusosp@users.noreply.github.com>
2020-11-25 20:56:45 -0300

    Added ip in Allowed hosts

--- backend/front/settings.py
+++ backend/front/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')y_yz7z3d@w!4i3bh9w_t%jbg8j_5c$)1=e407(az7xouo(_fh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.247.247.197', 'localhost', '127.0.0.1']
 
 
 # Application definition
