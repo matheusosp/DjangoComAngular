7223726c01e533790da34a6dc78dd6490556779c
matheusosp <matheusosp@users.noreply.github.com>
2020-11-26 00:21:40 -0300

    correcting tokens for production

--- front/src/app/members-detail/api.service.ts
+++ front/src/app/members-detail/api.service.ts
@@ -6,26 +6,26 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
 
-  baseUrl = 'http://35.247.247.197:8000/';
-  token = 'Token 2fbb4d84195097c87948a9132d478e2a054b9ccf'
+  baseUrl = 'http://35.247.247.197:8000';
+  token = 'Token 2fbb4d84195097c87948a9132d478e2a054b9ccf';
   httpHeaders = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', this.token);
 
   constructor(private http: HttpClient) { }
 
   getMember(id:number | string | null) : Observable<any>{
-    return this.http.get(` ${this.baseUrl}/members/${id}/`, 
+    return this.http.get(` ${this.baseUrl}members/${id}/`, 
       {headers: this.httpHeaders}
     );
   };
 
   updateMember(member: { name?: string; surname?: string;phone?:string;adress?:string; id?: number; }) : Observable<any>{
     let body = { name: member.name, surname:member.surname , phone:member.phone};
-    return this.http.put(this.baseUrl + '/members/'+member.id+'/', body,
+    return this.http.put(this.baseUrl + 'members/'+member.id+'/', body,
       {headers: this.httpHeaders}
     );
   };
   deleteMember(id:number): Observable<any>{
-    return this.http.delete(this.baseUrl + '/members/'+id+'/', 
+    return this.http.delete(this.baseUrl + 'members/'+id+'/', 
       {headers: this.httpHeaders}
     );
   }; 
